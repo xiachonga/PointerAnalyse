914e5e9ca3027eb7381a1c2ce1dc9187244ef31a
Chen Zhenchuan <chenzhenchuan16@mails.ucas.ac.cn>
2020-12-05 14:10:13 +0800

    Rename something

--- Liveness.h
+++ Liveness.h
@@ -26,6 +26,7 @@ using namespace llvm;
 using ValueSetType = std::set<Value *>;
 using PointToSetType = std::map<Value *, ValueSetType>;
 using ValueValuesMapType = std::map<Value *, ValueSetType>;
+using Functions = std::set<Function *>;
 
 //#define DEBUG
 struct PointerSetInfo
@@ -39,9 +40,25 @@ struct PointerSetInfo
 
     inline ValueSetType getPossibleValues(Value *V)
     {
-        if (isa<Function>(V) || isa<AllocaInst>(V))
+        // FIX ME
+        // add what malloc as what allocaInst do?
+        if (isa<Constant>(V) || isa<AllocaInst>(V))
             return {V};
         return PointToSet[V];
+    }
+
+    inline Functions getPossibleFuntcions(Value *V)
+    {
+        if (Function *func = dyn_cast<Function>(V))
+            return {func};
+        Functions funcs;
+        for (Value *temp : PointToSet[V])
+        {
+            Function *func = dyn_cast<Function>(temp);
+            assert(func && "The vaule must be a function!");
+            funcs.insert(func);
+        }
+        return funcs;
     }
 
     ValueSetType &operator[](Value *V)
@@ -87,6 +104,7 @@ public:
     {
         return finalResult;
     }
+
     void merge(PointerSetInfo *dest, const PointerSetInfo &src) override
     {
         for (auto iter = src.PointToSet.begin(); iter != src.PointToSet.end(); ++iter)
@@ -101,137 +119,117 @@ public:
             }
         }
     }
-    void computePhiNode(PHINode *phiNode, PointerSetInfo *dfval)
+    void computePhiNode(PHINode *phiNode, PointerSetInfo &info)
     {
         for (Use &U : phiNode->incoming_values())
         {
-            ValueSetType possibleValues = dfval->getPossibleValues(U.get());
-#ifdef DEBUG
-            if (possibleValues.size() != 0)
-                (*possibleValues.begin())->dump();
-#endif
-            (*dfval)[phiNode].insert(possibleValues.begin(), possibleValues.end());
+            ValueSetType possibleValues = info.getPossibleValues(U.get());
+            info[phiNode].insert(possibleValues.begin(), possibleValues.end());
         }
     }
 
-    void computeStoreInst(StoreInst *storeInst, PointerSetInfo *dfval)
+    void computeStoreInst(StoreInst *storeInst, PointerSetInfo &info)
     {
         Value *valueOp = storeInst->getValueOperand();
-        ValueSetType possibleValues = dfval->getPossibleValues(valueOp);
+        ValueSetType possibleValues = info.getPossibleValues(valueOp);
         Value *pointerOp = storeInst->getPointerOperand();
-        ValueSetType possiblePointers = dfval->getPossibleValues(pointerOp);
-
-        PointToSetType &pointToSet = dfval->PointToSet;
+        ValueSetType possiblePointers = info.getPossibleValues(pointerOp);
 
         if (possiblePointers.size() == 1)
         {
             Value *pointer = *possiblePointers.begin();
-            pointToSet[pointer] = possibleValues;
+            info[pointer] = possibleValues;
             return;
         }
         for (Value *pointer : possiblePointers)
         {
-            pointToSet[pointer].insert(possibleValues.begin(), possibleValues.end());
+            info[pointer].insert(possibleValues.begin(), possibleValues.end());
         }
     }
 
-    void computeLoadInst(LoadInst *loadInst, PointerSetInfo *dfval)
+    void computeLoadInst(LoadInst *loadInst, PointerSetInfo &info)
     {
         Value *pointerOp = loadInst->getPointerOperand();
-        ValueSetType possiblePointers = dfval->getPossibleValues(pointerOp);
+        ValueSetType possiblePointers = info.getPossibleValues(pointerOp);
 
-        PointToSetType &pointToSet = dfval->PointToSet;
         for (Value *pointer : possiblePointers)
         {
-            ValueSetType possibleValues = (*dfval)[pointer];
-            pointToSet[loadInst].insert(possibleValues.begin(), possibleValues.end());
+            ValueSetType possibleValues = info[pointer];
+            info[loadInst].insert(possibleValues.begin(), possibleValues.end());
         }
     }
 
-    void computeAllocaInst(AllocaInst *allocaInst, PointerSetInfo *dfval)
+    void computeBitCastInst(BitCastInst *bitCastInst, PointerSetInfo &info)
     {
-    }
-
-    void computeBitCastInst(BitCastInst *bitCastInst, PointerSetInfo *dfval)
-    {
-        //bitCastInst->dump();
         Value *valueOp = bitCastInst->getOperand(0);
-        ValueSetType possibleValues = dfval->getPossibleValues(valueOp);
+        ValueSetType possibleValues = info.getPossibleValues(valueOp);
         if (CallInst *callInst = dyn_cast<CallInst>(valueOp))
         {
-            (*dfval)[callInst] = {}; // FIX ME
+            if (Function *fmalloc = dyn_cast<Function>(callInst->getCalledValue()))
+            {
+                if (fmalloc->getName() == "malloc")
+                {
+                    // FIX ME
+                    // just use the callinst as the malloc return value
+                    // in llvm, we can create "malloc" IR with createMalloc in class CallInst
+                    info[bitCastInst] = {valueOp};
+                    return;
+                }
+            }
         }
-        (*dfval)[bitCastInst] = possibleValues;
+        valueOp->dump();
+        assert(0 && ">>>>> computeBitCastInst: operand not a malloc value!");
     }
 
-    void computeGetElementPtrInst(GetElementPtrInst *getElementPtrInst, PointerSetInfo *dfval)
+    void computeGetElementPtrInst(GetElementPtrInst *getElementPtrInst, PointerSetInfo &info)
     {
-        PointToSetType &pointToSet = dfval->PointToSet;
         Value *pointerOp = getElementPtrInst->getPointerOperand();
-#ifdef DEBUG
-        errs() << "=========PtrInst begin========"
-               << "\n";
-        pointerOp->dump();
-        errs() << "=========PtrInst end========"
-               << "\n";
-#endif
-        pointToSet[getElementPtrInst] = dfval->getPossibleValues(pointerOp);
+        info[getElementPtrInst] = info.getPossibleValues(pointerOp);
     }
 
-    void addArgument(Function *func, PointerSetInfo &currentInfo, Value *arg, ValueSetType possibleValues)
+    void addArgument(PointerSetInfo &argsInfo, PointerSetInfo &currentInfo, Value *arg, ValueSetType possibleValues)
     {
-        functionArgPointSet[func][arg].insert(possibleValues.begin(), possibleValues.end());
+        argsInfo[arg].insert(possibleValues.begin(), possibleValues.end());
         for (Value *value : possibleValues)
         {
             if (value == arg)
                 continue;
-            addArgument(func, currentInfo, value, currentInfo[value]);
+            addArgument(argsInfo, currentInfo, value, currentInfo[value]);
         }
     }
 
-    void computeCallInst(CallInst *callInst, PointerSetInfo *dfval)
+    void computeCallInst(CallInst *callInst, PointerSetInfo &info)
     {
         Value *value = callInst->getCalledValue();
-        ValueSetType possibleFunctions = dfval->getPossibleValues(value);
-        for (Value *temp : possibleFunctions)
+        Functions possibleFunctions = info.getPossibleFuntcions(value);
+        for (Function *func : possibleFunctions)
         {
-            Function *func = dyn_cast<Function>(temp);
-            assert(func && "the temp must be a function!");
             if (func->isIntrinsic())
-            {
                 return;
-            }
+            // Add function to result
             finalResult[callInst].insert(func);
-
-            (*dfval)[callInst].insert(returnPointSet[callInst].begin(), returnPointSet[callInst].end());
-            // We must get a return value from malloc, just use callinst?
-            // Fix me
+            // FIX ME
+            // We must get a return value from malloc, just use callinst, do it specilly in computeBitCastInst?
             if (func->getName() == "malloc")
-            {
-                (*dfval)[callInst] = {callInst};
                 return;
-            }
+            // get return value
+            info[callInst].insert(returnPointSet[callInst].begin(), returnPointSet[callInst].end());
+
             functionWorkList.insert(func);
-            if (callfunction.count(func) == 0)
-            {
-                callfunction[func] = {callInst};
-            }
-            else
-            {
-                callfunction[func].insert(callInst);
-            }
-            int i = 0;
+            callfunction[func].insert(callInst);
+            // add argument
+            int argc = 0;
             for (Use &U : callInst->args())
             {
                 Value *V = U.get();
-                Argument *arg = func->getArg(i);
-                addArgument(func, *dfval, arg, dfval->getPossibleValues(V));
-                ++i;
+                Argument *arg = func->getArg(argc);
+                addArgument(functionArgPointSet[func], info, arg, info.getPossibleValues(V));
+                argc++;
             }
         }
     }
 
-    void computeReturnInst(ReturnInst *returnInst, PointerSetInfo *dfval)
+    void computeReturnInst(ReturnInst *returnInst, PointerSetInfo *info)
     {
 //存函数可能有哪些callInst调用，然后根据函数找这些callInst，
 #ifdef DEBUG
@@ -251,7 +249,7 @@ public:
                 Value *returnValue = returnInst->getReturnValue();
                 for (std::set<CallInst *>::iterator iter = tempCallSet.begin(); iter != tempCallSet.end(); ++iter)
                 {
-                    if (dfval->PointToSet.count(returnValue) != 0)
+                    if (info->PointToSet.count(returnValue) != 0)
                     {
 #ifdef DEBUG
                         errs() << "======returnFunction begin======"
@@ -262,9 +260,9 @@ public:
                         errs() << "======returnFunction end======"
                                << "\n";
 #endif
-                        ValueSetType tempReturnValueSet = dfval->PointToSet[returnValue];
+                        ValueSetType tempReturnValueSet = info->PointToSet[returnValue];
                         //(*tempReturnValueSet.begin())->dump();
-                        //dfval->PointToSet[*iter].insert(tempReturnValueSet.begin(), tempReturnValueSet.end());
+                        //info->PointToSet[*iter].insert(tempReturnValueSet.begin(), tempReturnValueSet.end());
                         unsigned int size = returnPointSet[*iter].size();
                         //errs() << size << "\n";
                         returnPointSet[*iter].insert(tempReturnValueSet.begin(), tempReturnValueSet.end());
@@ -286,73 +284,55 @@ public:
 
     void compDFVal(Instruction *inst, PointerSetInfo *dfval) override
     {
+        PointerSetInfo &info = *dfval;
         if (isa<DbgInfoIntrinsic>(inst))
             return;
-        // errs() << ">>>>>>>>>>\t";
-        // inst->dump();
+#ifdef INFO
+        errs() << ">>>>> compDFVal\t";
+        inst->dump();
+#endif
         if (PHINode *phiNode = dyn_cast<PHINode>(inst))
         {
-#ifdef DEBUG
-            errs() << phiNode->getName() << "\n";
-#endif
-            computePhiNode(phiNode, dfval);
+            computePhiNode(phiNode, info);
         }
         else if (StoreInst *storeInst = dyn_cast<StoreInst>(inst))
         {
-#ifdef DEBUG
-            errs() << storeInst->getName() << "\n";
-#endif
-            computeStoreInst(storeInst, dfval);
+            computeStoreInst(storeInst, info);
         }
         else if (LoadInst *loadInst = dyn_cast<LoadInst>(inst))
         {
-#ifdef DEBUG
-            errs() << loadInst->getName() << "\n";
-#endif
-            computeLoadInst(loadInst, dfval);
+            computeLoadInst(loadInst, info);
         }
         else if (CallInst *callInst = dyn_cast<CallInst>(inst))
         {
-#ifdef DEBUG
-            errs() << "===CallInst===" << callInst->getName() << "\n";
-#endif
-            computeCallInst(callInst, dfval);
+            computeCallInst(callInst, info);
         }
         else if (ReturnInst *returnInst = dyn_cast<ReturnInst>(inst))
         { // call 和 return都会令目标基本块的值发生改变，现在还没想好怎么弄
-#ifdef DEBUG
-            errs() << "===ReturnInst===" << returnInst->getName() << "\n";
-#endif
-            computeReturnInst(returnInst, dfval);
+            computeReturnInst(returnInst, &info);
         }
         else if (GetElementPtrInst *getElementPtrInst = dyn_cast<GetElementPtrInst>(inst))
         {
-#ifdef DEBUG
-            errs() << "===GetElementPtrInst===" << getElementPtrInst->getName() << "\n";
-#endif
-            computeGetElementPtrInst(getElementPtrInst, dfval);
+            computeGetElementPtrInst(getElementPtrInst, info);
         }
+#ifdef DEBUG
         else if (AllocaInst *allocaInst = dyn_cast<AllocaInst>(inst))
         {
-            computeAllocaInst(allocaInst, dfval);
+
+            errs() << ">>>>> AllocaInst\t";
+            allocaInst->dump();
         }
+#endif
         else if (BitCastInst *bitCastInst = dyn_cast<BitCastInst>(inst))
         {
-            computeBitCastInst(bitCastInst, dfval);
+            computeBitCastInst(bitCastInst, info);
         }
         else
         {
 #ifdef DEBUG
-            errs() << inst->getName() << "\n";
+            errs() << ">>>!! Instruction need to handle\t";
+            inst->dump();
 #endif
         }
-        //dfval->LiveVars.erase(inst);
-        /*for(User::op_iterator oi = inst->op_begin(), oe = inst->op_end();
-            oi != oe; ++oi) {
-           Value * val = *oi;
-           if (isa<Instruction>(val)) 
-               dfval->LiveVars.insert(cast<Instruction>(val));
-       }*/
     }
 };
-
